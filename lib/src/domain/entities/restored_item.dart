89af44746ddae2cfe195abe735c1c54470ed1f9d
Wajdi Ben Moumen <wajdibbm@gmail.com>
2024-12-25 17:26:44 +0100

    redeploy

--- lib/src/domain/entities/restored_item.dart
+++ lib/src/domain/entities/restored_item.dart
@@ -10,5 +10,5 @@ abstract  class RestoredItem extends Equatable
   
   @override
   List<Object> get props => [id, itemId, quantity, description!];
-  // RestoredItem copyWith({
+  // RestoredItem copyWith({ // 
 }
